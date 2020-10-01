5e2cef5bc71c99ff375f4aabcb48f2af0c05b8e3
yurifws <33961354+yurifws@users.noreply.github.com>
2020-10-01 00:43:16 -0300

    Testando um client JavaScript com PKCE e Authorization Code

--- client.js
+++ client.js
@@ -1,97 +1,112 @@
+const config = {
+  clientId: "foodanalytics",
+  authorizeUrl: "http://localhost:8081/oauth/authorize",
+  tokenUrl: "http://localhost:8081/oauth/token",
+  callbackUrl: "http://127.0.0.1:5500/algafood-js/",
+  cozinhasUrl: "http://localhost:8080/v1/cozinhas"
+};
 
- const config = {
-    clientId: "foodanalytics",
-    clientSecret: "food123",
-    authorizeUrl: "http://localhost:8081/oauth/authorize",
-    tokenUrl: "http://localhost:8081/oauth/token",
-    callbackUrl: "http://127.0.0.1:5500/algafood-js/",
-    cozinhasUrl: "http://localhost:8080/v1/cozinhas"
-  };
+let accessToken = "";
+
+function generateCodeVerifier() {
+  let codeVerifier = generateRandomString(128);
+  localStorage.setItem("codeVerifier", codeVerifier);
+
+  return codeVerifier;
+}
+
+function generateRandomString(length) {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   
-  let accessToken = "";
-  
-  function consultar() {
-    alert("Consultando recurso com access token " + accessToken);
-  
-    $.ajax({
-      url: config.cozinhasUrl,
-      type: "get",
-  
-      beforeSend: function(request) {
-        request.setRequestHeader("Authorization", "Bearer " + accessToken);
-      },
-  
-      success: function(response) {
-        var json = JSON.stringify(response);
-        $("#resultado").text(json);
-      },
-  
-      error: function(error) {
-        alert("Erro ao consultar recurso");
-      }
-    });
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   
-  function gerarAccessToken(code) {
-    alert("Gerando access token com code " + code);
-  
-    let clientAuth = btoa(config.clientId + ":" + config.clientSecret);
-    
-    let params = new URLSearchParams();
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", config.callbackUrl);
-  
-    $.ajax({
-      url: config.tokenUrl,
-      type: "post",
-      data: params.toString(),
-      contentType: "application/x-www-form-urlencoded",
-  
-      beforeSend: function(request) {
-        request.setRequestHeader("Authorization", "Basic " + clientAuth);
-      },
-  
-      success: function(response) {
-        accessToken = response.access_token;
-  
-        alert("Access token gerado: " + accessToken);
-      },
-  
-      error: function(error) {
-        alert("Erro ao gerar access key");
-      }
-    });
-  }
-  
-  function login() {
-    // https://auth0.com/docs/protocols/oauth2/oauth-state
-    let state = btoa(Math.random());
-    localStorage.setItem("clientState", state);
-  
-    window.location.href = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&state=${state}&redirect_uri=${config.callbackUrl}`;
-  }
-  
-  $(document).ready(function() {
-    let params = new URLSearchParams(window.location.search);
-  
-    let code = params.get("code");
-    let state = params.get("state");
-    let currentState = localStorage.getItem("clientState");
-  
-    if (code) {
-      // window.history.replaceState(null, null, "/");
-  
-      if (currentState == state) {
-        gerarAccessToken(code);
-      } else {
-        alert("State inválido");
-      }
+  return text;
+}
+
+function generateCodeChallenge(codeVerifier) {
+  return base64URL(CryptoJS.SHA256(codeVerifier));
+}
+
+function getCodeVerifier() {
+  return localStorage.getItem("codeVerifier");
+}
+
+function base64URL(string) {
+  return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+function consultar() {
+  alert("Consultando recurso com access token " + accessToken);
+
+  $.ajax({
+    url: config.cozinhasUrl,
+    type: "get",
+
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
+
+    success: function(response) {
+      var json = JSON.stringify(response);
+      $("#resultado").text(json);
+    },
+
+    error: function(error) {
+      alert("Erro ao consultar recurso");
     }
   });
-  
-  $("#btn-consultar").click(consultar);
-  $("#btn-login").click(login);
+}
 
+function gerarAccessToken(code) {
+  alert("Gerando access token com code " + code);
+ 
+  let codeVerifier = getCodeVerifier();
 
+  let params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", config.callbackUrl);
+  params.append("client_id", config.clientId);
+  params.append("code_verifier", codeVerifier);
 
+  $.ajax({
+    url: config.tokenUrl,
+    type: "post",
+    data: params.toString(),
+    contentType: "application/x-www-form-urlencoded",
+
+    success: function(response) {
+      accessToken = response.access_token;
+
+      alert("Access token gerado: " + accessToken);
+    },
+
+    error: function(error) {
+      alert("Erro ao gerar access key");
+    }
+  });
+}
+
+function login() {
+  let codeVerifier = generateCodeVerifier();
+  let codeChallenge = generateCodeChallenge(codeVerifier);
+
+  window.location.href = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&redirect_uri=${config.callbackUrl}&code_challenge_method=s256&code_challenge=${codeChallenge}`;
+}
+
+$(document).ready(function() {
+  let params = new URLSearchParams(window.location.search);
+
+  let code = params.get("code");
+
+  if (code) {
+    // window.history.replaceState(null, null, "/");
+    gerarAccessToken(code);
+  }
+});
+
+$("#btn-consultar").click(consultar);
+$("#btn-login").click(login);
